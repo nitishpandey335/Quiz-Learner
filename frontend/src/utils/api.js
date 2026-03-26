a057dd0e98d6c4cc627891bae39a5a60762dd9ee
nitishpandey335 <nitishkumarpandey05@gmail.com>
2026-03-26 23:26:54 +0530

    commit changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -16,6 +16,9 @@ API.interceptors.request.use((config) => {
 export const registerUser = (data) => API.post('/auth/register', data);
 export const loginUser = (data) => API.post('/auth/login', data);
 export const getMe = () => API.get('/auth/me');
+export const sendOtp = (data) => API.post('/auth/send-otp', data);
+export const loginWithOtp = (data) => API.post('/auth/login-otp', data);
+export const resetPassword = (data) => API.post('/auth/reset-password', data);
 
 // Users (admin)
 export const getAllUsers = () => API.get('/users');
